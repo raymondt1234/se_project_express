0a3826e19d73b10d3d832b768faa829c499868bd
Raymond Terry <raymondt1234@gmail.com>
2025-11-09 12:23:39 -0700

    Fix: unused parameter warning by prefixing with underscore

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,4 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, _next) => {
   console.error(err);
 
   res.status(err.statusCode || 500).json({message: err.message || "An error has occurred on the server"});
